1f22ddab01b7f2f5629ae7d13a98bf561e633271
Yasas Rajika <it22324542@my.sliit.lk>
2025-03-22 13:27:27 +0530

    cors update

--- src/index.js
+++ src/index.js
@@ -7,8 +7,13 @@ import dbConnect from "./config/dbConnect.js";
 
 const app = express();
 
-app.use(cors());
-app.use(cors({ origin: "http://localhost:5173" }));
+//app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5175",
+  methods: "GET,POST,PUT,DELETE",
+  allowedHeaders: "Content-Type,Authorization",
+  credentials: true
+}));
 app.use(express.json());
 
 dbConnect();
@@ -17,8 +22,12 @@ app.use("/api/inventory", inventoryRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.get("/", (req, res) => {
-    res.json({ message: "Hello from backend!" });
-  });
+  try{
+    res.status(200).json({message : "hellow from backend"})
+  }catch(error){
+    console.log(error);
+  }
+});
   
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
